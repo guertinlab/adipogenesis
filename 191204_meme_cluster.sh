609ca1e838695200d195f9cbe8bb57dc86a731d1
mjg54 <guertinmj@gmail.com>
2019-12-06 16:31:13 -0500

    Update and rename 191122_meme_cluster_1.sh to 191204_meme_cluster.sh

---
+++ 191204_meme_cluster.sh
@@ -0,0 +1,14 @@
+cd ~/clusters
+for i in cluster_bed_cluster*.bed
+do
+    name=$(echo $i | awk -F"_bed_" '{print $NF}' | awk -F".bed" '{print $1}')
+    echo $name
+    slopBed -i cluster_bed_${name}.bed \                                                                                                                                                                                         
+       -g ../mm10.chrom.sizes -b -50 | \                                                                                                                                                                                         
+    fastaFromBed -fi /scratch/mjg7y/mm10.fa -bed stdin \                                                                                                                                                                         
+                -fo ${name}_test.fasta                                                                                                                                                                                           
+    head ${name}.fasta
+    srun meme -p 40 -oc ${name}_test.meme_output -nmotifs 15 -objfun classic -evt 0.01 -searchsize 0 -minw 6 \
+         -maxw 18 -revcomp -dna -markov_order 3 -maxsize 100000000  \
+         ${name}.fasta
+done
