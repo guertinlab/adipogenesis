1af038a47c380ae4fb23a1b6cc2ce91280f95d06
mjg54 <guertinmj@gmail.com>
2020-01-09 11:32:38 -0500

    Add files via upload

--- pro_dREG/191201_meme_pro_clusters.sh
+++ pro_dREG/191201_meme_pro_clusters.sh
@@ -9,7 +9,7 @@ do
         fastaFromBed -fi /scratch/mjg7y/mm10.fa -bed stdin \
 		 -fo ${name}_pro.fasta
     head ${name}_pro.fasta
-    meme -p 64 -oc ${name}.dREG_meme_output -objfun classic -nmotifs 15 -searchsize 0 -minw 6 \
+    srun meme -p 59 -oc meme -p 64 -oc ${name}.dREG_meme_output -objfun classic -evt 0.01 -nmotifs 15 -searchsize 0 -minw 6 \
 	 -maxw 18 -revcomp -dna -markov_order 3 -maxsize 100000000  \
          ${name}_pro.fasta
 done
