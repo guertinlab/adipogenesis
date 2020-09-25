a4ecd598e84d4df08a48a5c45420427310863002
bhn9by <66439197+bhn9by@users.noreply.github.com>
2020-09-25 16:18:20 -0700

    Update 1.1.0_alignment_generate_slurm.sh

--- Bao_code_chunks/1.1.0_alignment_generate_slurm.sh
+++ Bao_code_chunks/1.1.0_alignment_generate_slurm.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+###align .fastq to 10mm genome
+
 ###generate a unique slurm file for each replicate and run them in parallel
 #header_1   --> sbatch settings
 #temp.txt   --> name of .out file
 #header_2   --> modules to load
 #temp2.txt  --> name of relevant file
 #header_3   --> actual commands
-
-#align .fastq to 10mm genome
 for i in *_atac_PE1.fastq.gz
 do
     name=$(echo $i | awk -F"/" '{print $NF}' | awk -F"_atac_PE1.fastq.gz" '{print $1}')
