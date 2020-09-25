8777bc70094a469f8e6e531ad9ed4d04647273cc
bhn9by <66439197+bhn9by@users.noreply.github.com>
2020-09-25 16:41:21 -0700

    Update and rename peak_calling_script.sh to 1.4.0_peak_calling_new_approach.sh

--- Bao_code_chunks/1.4.0_peak_calling_new_approach.sh
+++ Bao_code_chunks/1.4.0_peak_calling_new_approach.sh
@@ -1,6 +1,17 @@
+#!/bin/bash
+
+#archive all files for 6d treatment; not used for analysis
 mkdir 6day
 mv *6d* 6day/
 
+###perform seqOutBias and convert .bam to .bigwig
+
+#generate a unique slurm file for each replicate and run them in parallel:
+#header_1   --> sbatch settings
+#temp.txt   --> name of .out file
+#header_2   --> modules to load
+#temp2.txt  --> name of relevant file
+#header_3   --> actual commands
 for bam in *rep1_atac_rmdup.bam
 do
     name=$(echo $bam | awk -F"_rep1_atac_rmdup.bam" '{print $1}')
@@ -9,6 +20,4 @@ do
     echo 'name='$name > temp2.txt
     cat peak_calling_slurm_header_1.txt temp.txt peak_calling_slurm_header_2.txt temp2.txt peak_calling_slurm_header_3.txt > $name.peak.calling.slurm
     sbatch $name.peak.calling.slurm
-    
 done
-
