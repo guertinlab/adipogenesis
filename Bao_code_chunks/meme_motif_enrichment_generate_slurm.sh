3a917f0ba0dd4aca3a5cee18ffacd40f3ff6aa2f
bhn9by <66439197+bhn9by@users.noreply.github.com>
2020-09-26 17:49:50 -0700

    Update and rename meme_script.sh to meme_motif_enrichment_generate_slurm.sh

--- Bao_code_chunks/meme_motif_enrichment_generate_slurm.sh
+++ Bao_code_chunks/meme_motif_enrichment_generate_slurm.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+
+#run MEME on each cluster to find enriched motifs
+wget https://hgdownload-test.gi.ucsc.edu/goldenPath/mm10/bigZips/mm10.chrom.sizes
+mkdir meme_motif_enrichment
+
+#generate a unique slurm file for each replicate and run them in parallel:
+#header_1   --> sbatch settings
+#temp.txt   --> name of .out file
+#header_2   --> modules to load
+#temp2.txt  --> name of relevant file
+#header_3   --> actual commands
 for i in *cluster*bed
 do
     name=$(echo $i | awk -F"_" '{print $NF}' | awk -F".bed" '{print $1}')
