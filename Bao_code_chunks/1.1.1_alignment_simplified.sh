8dc30ade2e8233208eaba2f6f11316a1a8f1f654
bhn9by <66439197+bhn9by@users.noreply.github.com>
2020-09-26 17:06:58 -0700

    Update 1.1.1_alignment_simplified.sh

--- Bao_code_chunks/1.1.1_alignment_simplified.sh
+++ Bao_code_chunks/1.1.1_alignment_simplified.sh
@@ -6,13 +6,16 @@ do
     name=$(echo $i | awk -F"/" '{print $NF}' | awk -F"_atac_PE1.fastq.gz" '{print $1}')
     echo $name
     gunzip $name*.gz
+    
     echo 'pairing .fastq files'
     fastq_pair ${name}_atac_PE1.fastq ${name}_atac_PE2.fastq
     rm $name*single.fq
+    
     echo 'align to mouse genome'
     bowtie2 --maxins 500 -x /project/genomes/Mus_musculus/UCSC/mm10/Sequence/Bowtie2Index/genome \
       -1 ${name}_atac_PE1.fastq.paired.fq \
       -2 ${name}_atac_PE2.fastq.paired.fq -S ${name}_atac_smp.sam
+      
     echo 'quality filter and remove duplicate amplicons'	
     samtools view -b -q 10 ${name}_atac_smp.sam | samtools sort -n - | \
         samtools fixmate -m - - | samtools sort - | samtools markdup -r - ${name}_atac_rmdup.bam
