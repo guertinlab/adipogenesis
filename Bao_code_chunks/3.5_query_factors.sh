8d9cdca6125d199dcb4f8923e027552ff5479109
bhn9by <66439197+bhn9by@users.noreply.github.com>
2020-10-19 20:34:41 -0700

    Update 3.5_query_factors.sh

--- Bao_code_chunks/3.5_query_factors.sh
+++ Bao_code_chunks/3.5_query_factors.sh
@@ -4,7 +4,7 @@
 #NOTE: This wget doesn't work and the code you get is also not up-to-date with python3
 #wget https://github.com/guertinlab/adipogenesis/blob/2fdf0bbab4fe6f368f5a60e42f7899b6570ff71c/motif_analysis/MEME_individual_from_db.py
 
-#See misc_script for corrected MEME_individual_from_db_python3.py
+#See misc_scripts directory for corrected MEME_individual_from_db_python3.py
 
 #extract individual meme files from combined database
 mkdir individual_memes
