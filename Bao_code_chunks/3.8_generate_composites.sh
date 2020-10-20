b3c5844efd80d6c130c2404b9543772d45402746
bhn9by <66439197+bhn9by@users.noreply.github.com>
2020-10-19 21:39:17 -0700

    Update 3.8_generate_composites.sh

--- Bao_code_chunks/3.8_generate_composites.sh
+++ Bao_code_chunks/3.8_generate_composites.sh
@@ -50,7 +50,7 @@ do
 	echo FINAL_QUERY IS $final_query
 	wc -l ${final_query}.tomtom_output/tomtom.tsv
 	cd ${final_query}.tomtom_output
-	python ../../../tomtom_output_to_composite.py -i tomtom.xml
+	python ../../../tomtom_output_to_composite_python3.py -i tomtom.xml
 	mv tomtom.xml_test_index_pswm.txt ../composite.values.txt
 	mv tomtom.xml_test_index_rc_offset.txt ../composite.index.txt
 	cd ../..
