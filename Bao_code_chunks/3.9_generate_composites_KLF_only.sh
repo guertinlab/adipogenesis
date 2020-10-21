87385c96c1f4d5f827d04cf119e4ac7b1ff7007f
bhn9by <66439197+bhn9by@users.noreply.github.com>
2020-10-20 21:03:30 -0700

    Update 3.9_generate_composites_KLF_only.sh

--- Bao_code_chunks/3.9_generate_composites_KLF_only.sh
+++ Bao_code_chunks/3.9_generate_composites_KLF_only.sh
@@ -23,28 +23,28 @@ done
 #query tomtom for each factor against all others
 module load gcc/9.2.0  mvapich2/2.3.3 meme/5.1.0
 cat ../klf.txt | { while read line
-	do
-	    query_factor=$line
-	    rm ref_factors_meme.txt
-	    mv ${query_factor}_meme.txt ..
-	    cat *_meme.txt > ref_factors_meme.txt
-	    mv ../${query_factor}_meme.txt $PWD
-	    tomtom -no-ssc -o ${query_factor}.tomtom_output -verbosity 1 -incomplete-scores -min-overlap 1 -dist ed -evalue -thresh 0.0005 ${query_factor}_meme.txt ref_factors_meme.txt
-	    
-	    if [[ $(wc -l < ${query_factor}.tomtom_output/tomtom.tsv) -ge $max_motif ]]
-	    then
-		max_motif=$(wc -l < ${query_factor}.tomtom_output/tomtom.tsv)
-		final_query=$query_factor
-	    fi
-	    
-	done
-	echo FINAL_QUERY IS $final_query
-	wc -l ${final_query}.tomtom_output/tomtom.tsv
-	cd ${final_query}.tomtom_output
-	python2.7 ../../tomtom_output_to_composite.py -i tomtom.xml
-	mv tomtom.xml_test_index_pswm.txt ../composite.values.txt
-	mv tomtom.xml_test_index_rc_offset.txt ../composite.index.txt
-	cd ..
+do
+    query_factor=$line
+    rm ref_factors_meme.txt
+    mv ${query_factor}_meme.txt ..
+    cat *_meme.txt > ref_factors_meme.txt
+    mv ../${query_factor}_meme.txt $PWD
+    tomtom -no-ssc -o ${query_factor}.tomtom_output -verbosity 1 -incomplete-scores -min-overlap 1 -dist ed -evalue -thresh 0.0005 ${query_factor}_meme.txt ref_factors_meme.txt
+
+    if [[ $(wc -l < ${query_factor}.tomtom_output/tomtom.tsv) -ge $max_motif ]]
+    then
+	max_motif=$(wc -l < ${query_factor}.tomtom_output/tomtom.tsv)
+	final_query=$query_factor
+    fi
+
+done
+echo FINAL_QUERY IS $final_query
+wc -l ${final_query}.tomtom_output/tomtom.tsv
+cd ${final_query}.tomtom_output
+python2.7 ../../tomtom_output_to_composite.py -i tomtom.xml
+mv tomtom.xml_test_index_pswm.txt ../composite.values.txt
+mv tomtom.xml_test_index_rc_offset.txt ../composite.index.txt
+cd ..
 }
 
 module purge
