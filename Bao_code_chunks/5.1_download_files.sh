3b109732c6fd39241b50c29f302150d90c2e020f
bhn9by <66439197+bhn9by@users.noreply.github.com>
2021-01-22 23:53:48 -0800

    Create 5.1_download_files.sh

---
+++ Bao_code_chunks/5.1_download_files.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /scratch/bhn9by/ATAC/
+wget https://raw.githubusercontent.com/guertinlab/adipogenesis/master/Bao_code_chunks/misc_scripts/generate_composite_motif.SP.R
+wget https://raw.githubusercontent.com/guertinlab/adipogenesis/master/Bao_code_chunks/misc_scripts/generate_composite_motif.KLF.R
