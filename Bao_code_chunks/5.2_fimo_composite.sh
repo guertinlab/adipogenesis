105103033f29d76041758148e86f6144d7efcfe0
bhn9by <66439197+bhn9by@users.noreply.github.com>
2021-01-22 21:13:42 -0800

    Update 5.2_fimo_composite.sh

--- Bao_code_chunks/5.2_fimo_composite.sh
+++ Bao_code_chunks/5.2_fimo_composite.sh
@@ -1,1 +1,14 @@
+rm -r fimo_composites
+mkdir fimo_composites
 
+for i in PSWM*txt
+do
+    name=$(echo $i | awk -F".txt" '{print $1}')
+    echo $name
+    echo '#SBATCH -o' $name'.fimo.out' > temp.txt
+    echo 'i='../composite_motifs/$name/${name}_meme.txt > temp2.txt
+    cat fimo_slurm_header_1.txt temp.txt fimo_slurm_header_2.txt temp2.txt fimo_slurm_header_3.txt > $name.fimo.slurm
+    #sbatch $name.fimo.slurm                                                                                                                                                           
+    rm temp.txt
+    rm temp2.txt
+done
