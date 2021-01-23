4c276393ffb388a483f41ecf2d593d88f6bea428
bhn9by <66439197+bhn9by@users.noreply.github.com>
2021-01-22 22:58:47 -0800

    Update 4.3_generate_composites_part2_ceqlogo.sh

--- Bao_code_chunks/4.3_generate_composites_part2_ceqlogo.sh
+++ Bao_code_chunks/4.3_generate_composites_part2_ceqlogo.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-cd /scratch/bhn9by/ATAC
-
-wget https://raw.githubusercontent.com/guertinlab/adipogenesis/master/Bao_code_chunks/misc_scripts/generate_composite_motif.R
-wget https://raw.githubusercontent.com/guertinlab/adipogenesis/master/Bao_code_chunks/misc_scripts/meme_header.txt
-
 for family in PSWM*
 do
     cd $family
