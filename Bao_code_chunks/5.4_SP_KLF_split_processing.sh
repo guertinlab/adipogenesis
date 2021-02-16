6b119875989746377b855e94d57ce841a6854a06
bhn9by <66439197+bhn9by@users.noreply.github.com>
2021-02-15 19:49:37 -0800

    Update 5.4_SP_KLF_split_processing.sh

--- Bao_code_chunks/5.4_SP_KLF_split_processing.sh
+++ Bao_code_chunks/5.4_SP_KLF_split_processing.sh
@@ -2,10 +2,11 @@
 
 cd /scratch/bhn9by/ATAC/SP_KLF_split
 
-#bedtools getfasta -fo sp.klf.fasta -fi /project/genomes/Mus_musculus/UCSC/mm10/Sequence/WholeGenomeFasta/genome.fa -bed /scratch/bhn9by/ATAC/fimo_composites/PSWM_family_8_fimo.bed
 module load gcc/7.1.0  openmpi/3.1.4 R/4.0.0
 
 echo Starting prep R script
+
+#Generate sp_fimo.txt, sp_fimo_nondyn.txt, and sp_klf_2M.txt
 Rscript ../prep.SP.KLF.fimo.R
 
 module load gcc/9.2.0  mvapich2/2.3.3 meme/5.1.0
@@ -16,7 +17,7 @@ echo Starting KLF FIMO
 fimo --thresh 0.01 --text KLF/KLF_composite_meme.txt sp_fimo.txt > output_klf.txt
 
 module load gcc/7.1.0 bedtools/2.26.0
-cp /scratch/bhn9by/ATAC/fimo_composites/PSWM_family_8_2M.bed $PWD/sp_klf_2M.bed
+cp /scratch/bhn9by/ATAC/fimo_composites/PSWM_family_7_2M.bed $PWD/sp_klf_2M.bed
 bedtools getfasta -fi /project/genomes/Mus_musculus/UCSC/mm10/Sequence/WholeGenomeFasta/genome.fa -bed sp_klf_2M.bed > sp_klf_2M.fasta
 
 module load gcc/9.2.0  mvapich2/2.3.3 meme/5.1.0
@@ -26,6 +27,8 @@ fimo --thresh 0.01 --text KLF/KLF_composite_meme.txt sp_klf_2M.txt > output_klf_
 
 echo Starting split R script
 module load gcc/7.1.0  openmpi/3.1.4 R/4.0.0
+
+#Generate SP_unsorted.bed and KLF_unsorted.bed
 Rscript ../SP_KLF_split.R
 
 echo DONE
