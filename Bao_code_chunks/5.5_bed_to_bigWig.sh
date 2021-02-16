bd89deadcf3cced49640f1ca6b272fd2b7fdb61d
bhn9by <66439197+bhn9by@users.noreply.github.com>
2021-02-15 19:38:05 -0800

    Create 5.5_bed_to_bigWig.sh

---
+++ Bao_code_chunks/5.5_bed_to_bigWig.sh
@@ -0,0 +1,38 @@
+#prepare bigWigs for motif enrichment plot
+cd /scratch/bhn9by/ATAC/fimo_composites/main_figure_beds
+
+cat SP_unsorted.bed | sort -k1,1 -k2,2n > SP_2M.bed
+cat KLF_unsorted.bed | sort -k1,1 -k2,2n > KLF_2M.bed
+
+module load ucsc-tools/3.7.4 gcc/9.2.0 bedtools/2.29.2
+
+intersectBed -loj -a ../../all_peaks.bed -b SP_2M.bed > ../SP_fimo_all.bed
+intersectBed -loj -a ../../all_peaks.bed -b KLF_2M.bed > ../KLF_fimo_all.bed
+
+intersectBed -loj -a ../../dynamic_peaks.bed -b SP_2M.bed > SP_fimo.bed
+intersectBed -loj -a ../../dynamic_peaks.bed -b KLF_2M.bed > KLF_fimo.bed
+
+for bed in *2M.bed
+do
+    name=$(echo $bed | awk -F"/" '{print $NF}' | awk -F"_2M.bed" '{print $1}')
+    echo $name
+    #summing scores of motifs w/in overlapping genomic interval
+    cat $bed | mergeBed -i stdin -c 4 -o sum > ${name}_merged_2M.bed
+    bedGraphToBigWig ${name}_merged_2M.bed ../../mm10.chrom.sizes ${name}_mm10_instances.bigWig
+    rm ${name}_merged_2M.bed
+done
+
+#extract up / down composite motifs from combined SP.KLF
+cd /scratch/bhn9by/Adipogenesis/ATAC/SP_KLF_split
+
+module load gcc/7.1.0  openmpi/3.1.4 R/4.0.0
+
+Rscript ../extract.motifs.from.combined.family.R
+
+module load gcc/7.1.0 meme/4.10.2
+
+ceqlogo -i SP.KLF_activated.txt -m SP.KLF_activated -o SP.KLF.activated.eps
+ceqlogo -i SP.KLF_activated.txt -m SP.KLF_activated -o SP.KLF.activated.rc.eps -r
+
+ceqlogo -i SP.KLF_repressed.txt -m SP.KLF_repressed -o SP.KLF.repressed.eps
+ceqlogo -i SP.KLF_repressed.txt -m SP.KLF_repressed -o SP.KLF.repressed.rc.eps -r
