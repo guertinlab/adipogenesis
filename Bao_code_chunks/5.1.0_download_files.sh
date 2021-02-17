46ac23e2a341087c379ec6ab4dd3cca139d91dc6
bhn9by <66439197+bhn9by@users.noreply.github.com>
2021-02-16 20:56:26 -0800

    Update 5.1.0_download_files.sh

--- Bao_code_chunks/5.1.0_download_files.sh
+++ Bao_code_chunks/5.1.0_download_files.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 cd /scratch/bhn9by/ATAC/
+#for main figure
 wget https://raw.githubusercontent.com/guertinlab/adipogenesis/master/Bao_code_chunks/misc_scripts/generate_composite_motif.SP.R
 wget https://raw.githubusercontent.com/guertinlab/adipogenesis/master/Bao_code_chunks/misc_scripts/generate_composite_motif.KLF.R
 wget https://raw.githubusercontent.com/guertinlab/adipogenesis/master/Bao_code_chunks/misc_scripts/prep.SP.KLF.fimo.R
 wget https://raw.githubusercontent.com/guertinlab/adipogenesis/master/Bao_code_chunks/misc_scripts/SP_KLF_split.R
+wget https://raw.githubusercontent.com/guertinlab/adipogenesis/master/Bao_code_chunks/misc_scripts/extract.motifs.from.combined.family.R
+
+#for supplemental figure
+wget https://github.com/guertinlab/adipogenesis/blob/master/Bao_code_chunks/misc_scripts/post.composite.fimo.supp.R
+wget https://github.com/guertinlab/adipogenesis/blob/master/Bao_code_chunks/misc_scripts/plot.motif.enrichment.supp.R
