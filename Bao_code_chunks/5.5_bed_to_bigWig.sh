a6d5fec41c1577ecd8fa817ee303f809effda031
bhn9by <66439197+bhn9by@users.noreply.github.com>
2021-02-17 00:06:09 -0800

    Update 5.5_bed_to_bigWig.sh

--- Bao_code_chunks/5.5_bed_to_bigWig.sh
+++ Bao_code_chunks/5.5_bed_to_bigWig.sh
@@ -23,7 +23,7 @@ do
 done
 
 #extract up / down composite motifs from combined SP.KLF
-cd /scratch/bhn9by/Adipogenesis/ATAC/SP_KLF_split
+cd /scratch/bhn9by/ATAC/SP_KLF_split
 
 module load gcc/7.1.0  openmpi/3.1.4 R/4.0.0
 
