f9523169d0cad26a0b4ed610a0f3f29a0afce2c9
bhn9by <66439197+bhn9by@users.noreply.github.com>
2021-02-17 12:50:40 -0800

    Create F.1.0_download_files.sh

---
+++ Bao_code_chunks/F.1.0_download_files.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /scratch/bhn9by/ATAC
+wget https://github.com/guertinlab/adipogenesis/blob/master/Bao_code_chunks/misc_scripts/post.composite.fimo.supp.R
+wget https://github.com/guertinlab/adipogenesis/blob/master/Bao_code_chunks/misc_scripts/plot.motif.enrichment.supp.R
