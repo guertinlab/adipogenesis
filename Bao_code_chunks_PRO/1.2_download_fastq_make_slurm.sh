ccdfcbded1b7c8345f8c3c702fe035d2a1c44575
bhn9by <66439197+bhn9by@users.noreply.github.com>
2021-03-01 17:08:06 -0800

    Update 1.2_download_fastq_make_slurm.sh

--- Bao_code_chunks_PRO/1.2_download_fastq_make_slurm.sh
+++ Bao_code_chunks_PRO/1.2_download_fastq_make_slurm.sh
@@ -1,1 +1,28 @@
+#On Rivanna
+mkdir /scratch/bhn9by/PRO
+cd /scratch/bhn9by/PRO
 
+#Upload sra.metadata.csv and SRR_Acc_List.txt to PRO directory
+
+#Set up conda env to get access to sratoolkit
+module load bioconda
+conda create -n myenv2 -c bioconda ucsc-bedgraphtobigwig ucsc-bigwigmerge
+
+#get mm10 tallymer files for seqOutBias
+cp ../ATAC/genome* $PWD
+
+#download .fastq from SRA
+#make a unique slurm file for each replicate and run them in parallel
+cat SRR_Acc_List.txt | while read acc
+do
+echo $acc
+echo '#SBATCH -o' $acc'.out' > temp.txt
+echo 'fasterq-dump' $acc > temp2.txt
+echo 'gzip' $acc'.fastq' > temp3.txt
+cat sra_slurm_header_1.txt temp.txt sra_slurm_header_2.txt temp2.txt temp3.txt > $acc.slurm
+sbatch $acc.slurm
+rm temp.txt
+rm temp2.txt
+rm temp3.txt
+
+done
