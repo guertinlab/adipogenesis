534f12f1f9fc826081dc281a27874695993761ea
bhn9by <66439197+bhn9by@users.noreply.github.com>
2021-03-03 00:02:46 -0800

    Update 1.3.0_install_packages.sh

--- Bao_code_chunks_PRO/1.3.0_install_packages.sh
+++ Bao_code_chunks_PRO/1.3.0_install_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Retrieve packages and put on the $PATH
+#Retrieve packages and put on $PATH
 
 #cutadapt
 python3 -m pip install --user --upgrade cutadapt
@@ -14,3 +14,10 @@ tar -xjf fastx_toolkit_0.0.13_binaries_Linux_2.6_amd64.tar.bz2
 cp ./bin/* /home/bhn9by/bin
 
 #fqdedup
+#local
+brew install rust
+tar xzf fqdedup-1.0.0.tar.gz
+cd fqdedup-1.0.0
+cargo build --release 
+
+#upload binary to Rivanna and put on $PATH
