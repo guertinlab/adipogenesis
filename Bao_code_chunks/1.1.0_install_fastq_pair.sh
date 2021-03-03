6975957d6ce191d62a5255e475d87aa8f52c0b77
bhn9by <66439197+bhn9by@users.noreply.github.com>
2021-03-03 01:46:40 -0800

    Update 1.1.0_install_fastq_pair.sh

--- Bao_code_chunks/1.1.0_install_fastq_pair.sh
+++ Bao_code_chunks/1.1.0_install_fastq_pair.sh
@@ -4,9 +4,8 @@
 #Install fastq_pair and put on $PATH
 #fastq_pair is not required for adipogenesis project but we use it redundantly for our ATAC pipeline
 
-wget https://github.com/linsalrob/fastq-pair/archive/master.zip
-unzip master.zip
-cd fastq-pair-master
+git clone https://github.com/linsalrob/fastq-pair.git
+cd fastq-pair
 gcc -std=gnu99 main.c robstr.c fastq_pair.c is_gzipped.c -o fastq_pair
 cp fastq_pair /home/bhn9by/bin
 cd..
