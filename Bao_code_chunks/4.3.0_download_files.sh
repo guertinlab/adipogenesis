e302e77ea4dd1940f14d315272e2d496b9b2b424
bhn9by <66439197+bhn9by@users.noreply.github.com>
2021-03-19 21:19:59 -0700

    Delete Bao_code_chunks directory

--- Bao_code_chunks/4.3.0_download_files.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /scratch/bhn9by/ATAC
-wget https://raw.githubusercontent.com/guertinlab/adipogenesis/master/Bao_code_chunks/misc_scripts/tomtom_output_to_composite.py
-wget https://raw.githubusercontent.com/guertinlab/adipogenesis/master/Bao_code_chunks/misc_scripts/meme_header.txt
-wget https://raw.githubusercontent.com/guertinlab/adipogenesis/master/Bao_code_chunks/misc_scripts/generate_composite_motif.R
