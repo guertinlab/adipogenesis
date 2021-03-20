cd818a9071efc2c798dd07d1145556ce4e659c7d
bhn9by <66439197+bhn9by@users.noreply.github.com>
2021-03-19 22:14:19 -0700

    Update 1.3_setting_up.sh

--- Pipeline_PRO/1.3_setting_up.sh
+++ Pipeline_PRO/1.3_setting_up.sh
@@ -12,7 +12,7 @@ conda create -n myenv2 -c bioconda ucsc-bedgraphtobigwig ucsc-bigwigmerge
 python3 -m pip install --user --upgrade cutadapt
 cp /home/bhn9by/.local/bin/cutadapt /home/bhn9by/bin
 
-#fastx-toolkit
+#fastx-toolkit (need fastx_trimmer, fastx_reverse_complement)
 mkdir fastx_bin
 cd fastx_bin
 wget http://hannonlab.cshl.edu/fastx_toolkit/fastx_toolkit_0.0.13_binaries_Linux_2.6_amd64.tar.bz2
