36b738a457f75f253194cca41039d839372687a4
bhn9by <66439197+bhn9by@users.noreply.github.com>
2021-03-19 21:18:02 -0700

    Delete pipeline_ATAC directory

--- pipeline_ATAC/3.1.0_download_files.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd /scratch/bhn9by/ATAC
-wget https://raw.githubusercontent.com/guertinlab/adipogenesis/master/Bao_code_chunks/misc_scripts/fimo_motif_enrichment.R
--- pipeline_ATAC/F.1.0_download_files.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /scratch/bhn9by/ATAC
-wget https://github.com/guertinlab/adipogenesis/blob/master/Bao_code_chunks/misc_scripts/post.composite.fimo.supp.R
-wget https://github.com/guertinlab/adipogenesis/blob/master/Bao_code_chunks/misc_scripts/plot.motif.enrichment.supp.R
